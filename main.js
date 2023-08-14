7e0edd5135420979cb101d4a8a58fab6564a9b1d
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2023-08-14 00:28:34 -0700

    grid configured
    fixed grid to display 256 squares

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const divContainer = document.getElementById('main-container');
-
+divContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+divContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 
 divMaker()
 
 function divMaker (){
-    let div = document.createElement('div');
-    divContainer.append("Hello");
-
+    for(i = 0; i < 256; i++){
+        let div = document.createElement('div');
+        div.classList.add('squares');
+        divContainer.appendChild(div)
+    }
 }
+
