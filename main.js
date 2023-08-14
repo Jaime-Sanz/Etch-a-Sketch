caed1f1f4787cc3ffdae36d44592494126581f13
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2023-08-13 17:15:06 -0700

    files fixed
    fixed files to allow javascript to run

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const divContainer = document.getElementById('main-container');
 
+
+
+divMaker()
+
+function divMaker (){
+    let div = document.createElement('div');
+    divContainer.append("Hello");
+
+}
